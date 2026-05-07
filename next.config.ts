607b3b0ef7301349c43cb7de2bfe0a8dfbff9426
zntb <zenetibi@yahoo.com>
2026-05-07 20:11:34 +0300

    feat: support image URLs in link icon field with full-card background display

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next';
-
+/** @type {*} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
